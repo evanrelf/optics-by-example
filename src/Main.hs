29d56874621fd1390a92df72bb98ef7f49ff89d9
Evan Relf <evan@evanrelf.com>
2019-12-28 15:36:36 -0800

    Add dependencies and language extensions

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,21 @@
-module Main (main) where
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+{- HLINT ignore "Unused LANGUAGE pragma" -}
+
+module Main where
+
+import Control.Applicative
+import Control.Lens
+import Data.Char
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.Text as Text
 
 main :: IO ()
 main = putStrLn "Hello world"
